3346e37525e0f607064bd6c0af586c78988cf0b5
HyunsolChoi <lasang2@naver.com>
2024-11-09 02:24:26 +0900

    Rebuild Modify Router -> BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Navigate, BrowserRouter} from 'react-router-dom';
 import SignIn from './signIn/SignIn';
 
 function App() {
 
     return (
-        <Router basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Navigate to="/signin" />} />
                 <Route path="/signin" element={<SignIn/>} />
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
 
